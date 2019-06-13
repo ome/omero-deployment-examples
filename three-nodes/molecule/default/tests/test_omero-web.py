40a3d19bef45abf1938738d3d55c301713d45961
Sebastien Besson <seb.besson@gmail.com>
2019-06-13 15:17:44 +0100

    Upgrade three-nodes to Molecule 2

--- three-nodes/molecule/default/tests/test_omero-web.py
+++ three-nodes/molecule/default/tests/test_omero-web.py
@@ -1,20 +1,21 @@
 import testinfra.utils.ansible_runner
+import os
 import pytest
 from time import sleep
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('omero-web')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('omero-web')
 
 
 @pytest.mark.parametrize('name', ['omero-web', 'nginx'])
-def test_services_running_and_enabled(Service, name):
-    service = Service(name)
+def test_services_running_and_enabled(host, name):
+    service = host.service(name)
     assert service.is_running
     assert service.is_enabled
 
 
-def test_omero_web_first_page(Command):
-    out = Command.check_output('curl -fsL http://localhost/')
+def test_omero_web_first_page(host):
+    out = host.check_output('curl -fsL http://localhost/')
     assert 'omero:4064' in out
 
 
@@ -29,14 +30,14 @@ def get_cookie(cookietxt, name):
 
 
 # https://github.com/openmicroscopy/omero-grid-docker/blob/0.1.0/test_login.sh
-def test_omero_web_login(Command, File):
+def test_omero_web_login(host):
     LOGIN_URL = 'http://localhost/webclient/login/'
     CURL = 'curl -f -i -k -s -c cookies.txt -b cookies.txt -e %s' % LOGIN_URL
 
-    for i in xrange(60):
+    for i in range(60):
         sleep(2)
-        Command.check_output('%s %s' % (CURL, LOGIN_URL))
-        csrf = get_cookie(File('cookies.txt').content_string, 'csrftoken')
+        host.check_output('%s %s' % (CURL, LOGIN_URL))
+        csrf = get_cookie(host.file('cookies.txt').content_string, 'csrftoken')
         if csrf:
             break
     assert csrf
@@ -49,12 +50,12 @@ def test_omero_web_login(Command, File):
         'url=%2Fwebclient%2F',
         ])
 
-    for i in xrange(60):
+    for i in range(60):
         sleep(2)
-        Command.check_output('%s -d "%s" -X POST %s' % (
+        host.check_output('%s -d "%s" -X POST %s' % (
             CURL, data, LOGIN_URL))
         sessionid = get_cookie(
-            File('cookies.txt').content_string, 'sessionid')
+            host.file('cookies.txt').content_string, 'sessionid')
         if sessionid:
             break
     assert sessionid
