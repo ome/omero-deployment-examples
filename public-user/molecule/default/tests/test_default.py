96dcf9cd852bea3d6e35f60c2aaa85a6a4a0ebaa
Sebastien Besson <seb.besson@gmail.com>
2019-06-14 14:48:37 +0100

    Address comments from Simon

--- public-user/molecule/default/tests/test_default.py
+++ public-user/molecule/default/tests/test_default.py
@@ -6,9 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-OMERO = '/opt/omero/server/OMERO.server/bin/omero'
-
-
 def test_omero_web_public(host):
     out = host.check_output(
         'curl -f http://localhost/webclient/api/containers/')
